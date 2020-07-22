e934aa0d96332382b4aad5e6451c636a7932379a
yte0 <Yohannes.Tesfay@helse-nord.no>
2020-07-22 14:25:01 +0200

    theme and colors

---
+++ src/page_colors.js
@@ -0,0 +1,30 @@
+export const page_colors = {
+  chart_colors: [
+    '#4F9A94',
+    '#90CAF9',
+    '#B0BEC5',
+    '#FFE082',
+    '#2962FF',
+    '#CE93D8',
+    '#9C786C',
+    '#BCAAA4',
+    '#F8BBD0',
+    '#9FA8DA',
+    '#80DEEA',
+    '#A5D6A7',
+    '#E6EE9C',
+    '#FFAB91',
+    '#78909C'
+  ],
+  line_color: '#D2D3D4',
+  background_color: '#EEF6F7',
+  primary_text_color: '#2D3034',
+  secondary_text_color: '#828586',
+  primary_color: '#7EBEC7',
+  secondary_color: '#00263D',
+  traffic_light_colors: {
+    high: '#BFE389',
+    mid: '#F%A623',
+    low: '#F04157'
+  }
+}
