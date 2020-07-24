439969f920018fb26836fedd3c5916fd86079e0d
yte0 <Yohannes.Tesfay@helse-nord.no>
2020-07-24 10:42:26 +0200

    rm dummy func and test

--- test/add.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from './utils.js';
-import add  from '../src/index.js'
-
-describe('The func add', function () {
-  it('works for 1 and 2', function () {
-    expect(add(1,2)).to.equal(3);
-  })
-})
