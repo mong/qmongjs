caac95bba3f22192ba38db7f1f12a154a98702ab
yte0 <Yohannes.Tesfay@helse-nord.no>
2020-10-22 14:02:06 +0200

    nsm logo and adjust size

--- src/components/header_text.js
+++ src/components/header_text.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const HEADER_TEXT = () => {
-  const text = "Her får du en samlet oversikt over kvalitetsindikatorer fra medisinske kvalitetsregistre tilknyttet hver helseregion, helseforetak og hvert enkelt sykehus. I tillegg til å se alle kvalitetsindikatorer for hvert enkelt sykehus kan du sortere på ulike fagområder."
+ 
 
   return(
-    <p className = "header_text">
-      {text}
-    </p>
+    <h1 className = "header_text"
+      style= {{fontSize: "2.7rem"}} >
+      {"Resultater fra medisinske kvalitetsregistre"}
+    </h1>
   )
 }
 export default HEADER_TEXT;
