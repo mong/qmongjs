6fc9fec44e13ca1e4678da2194b0c243a3cda1c7
YT <54272616+yte0@users.noreply.github.com>
2020-10-23 09:06:29 +0200

    update logo (#34)
    * logo clickable
    
    * minor changes to top header

--- src/components/header_main.js
+++ src/components/header_main.js
@@ -4,20 +4,27 @@ import HEADER_LOGO_SKDE from './header_logo_skde'
 import HEADER_TEXT from './header_text'
 
 const HEADER = () => {
-  const style = {backgroundImage:"linear-gradient(-270deg, #A2CFD6 0%, #7EBEC7 100%)", padding: "1rem",  color: "#2D3034"}
+  const style = {
+    backgroundImage:"linear-gradient(-270deg, #A2CFD6 0%, #7EBEC7 100%)",
+    padding: "1rem 0 0 1rem", 
+    color: "#2D3034",
+    display: "flex",
+    flexDirection: "column"
+  }
   return(
-  
     <div style = {style}>
       <div style= {{display:"flex", justifyContent: "space-between"}}>
-        <div style={{ width: "250px" }}> <HEADER_LOGO_SKDE /> </div>
-        <div style={{maxWidth: "200px"}}> <HEADER_LOGO_NSM /> </div>
+        <div style={{ width: "230px" }}>
+          <a href= "http://skde.no/"> <HEADER_LOGO_SKDE /></a> 
+        </div>
+        <div style={{width: "230px"}}> 
+          <a href= "https://www.kvalitetsregistre.no/"> <HEADER_LOGO_NSM /></a>
+        </div>
       </div>
-      <div style={{fontSize: "1.3rem", color:"white", fontSize:"2.8rem", lineHeight:"1.5", position: "relative", top: "-120px", padding: "0 0 0 25%", maxHeight:"20px"}}>
-          <HEADER_TEXT />
+      <div style={{color:"white", fontSize:"2.8rem", padding: "0 0 10px 17%"}}>
+        <HEADER_TEXT />
       </div>
-    </div>
-     
-    
+    </div>    
   )
 } 
 
--- src/components/header_text.js
+++ src/components/header_text.js
@@ -1,11 +1,8 @@
 import React from 'react'
 
 const HEADER_TEXT = () => {
- 
-
   return(
-    <h1 className = "header_text"
-      style= {{fontSize: "2.7rem"}} >
+    <h1 className = "header_text" style= {{fontSize: "2.7rem"}} >
       {"Resultater fra medisinske kvalitetsregistre"}
     </h1>
   )
