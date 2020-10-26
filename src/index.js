d00da4ba9db49c964a67533f94a1ec79344d36a1
yte0 <Yohannes.Tesfay@helse-nord.no>
2020-10-26 20:35:02 +0100

    update data using addCustomMessageHandler of shiny

--- src/index.js
+++ src/index.js
@@ -3,21 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
-
-const indicator_hosp = window.indicator_hosp
-const indicator_hf = window.indicator_hf
-const indicator_rhf= window.indicator_rhf
-const indicator_nation = window.indicator_nat
-const description = window.description
-const tu_names = window.tu_names
-
-
 ReactDOM.render(
   //<React.StrictMode>
-    <App 
-      data = {{indicator_hf, indicator_rhf , indicator_hosp, indicator_nation,tu_names, description}}
-      />,
+    <App />,
   //</React.StrictMode>,
   document.getElementById('root')
 );
