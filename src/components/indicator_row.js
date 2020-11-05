d830dd3b1c0f95d0d2a9dd1af66b0d72c05a756a
yte0 <Yohannes.Tesfay@helse-nord.no>
2020-11-05 11:00:00 +0100

    rm duplicatin

--- src/components/indicator_row.js
+++ src/components/indicator_row.js
@@ -13,7 +13,6 @@ const { data_config } = app_config
 
 function INDICATOR_ROW(props) {
   const {
-    mid_text = "av",
     data,
     treatment_unit_name = [],
     med_field_class = "",
@@ -35,86 +34,59 @@ function INDICATOR_ROW(props) {
       const ind_per_unit =data.agg_data.filtered_by_year.filter(
         data => data[data_config.column.treatment_unit] === tr_unit
       ) 
-    console.log(ind_per_unit)
-    if (ind_per_unit.length < 1){
-      return <td
-        key = {`${ind_id}_${tr_unit}_${index}_su`}
-        className = "selected_unit">
-          <NO_DATA/>
-      </td>
-    } else if (ind_per_unit[0][data_config.column.coverage_id] < 0.6){
-       //|| typeof(ind_per_unit[0][data_config.column.coverage_id]) === "undefined") {
-      return <td
-        key = {`${ind_id}_${tr_unit}_${index}_su`}
-        className = "selected_unit">
-          <LOW_COV/>
-      </td>
-    } else if (ind_per_unit[0][data_config.column.denominator] <
-       description[data_config.column.min_denominator]) {
-      return <td
-        key = {`${ind_id}_${tr_unit}_${index}_su`}
-        className = "selected_unit">
-          <LOW_N/>
-      </td>
-    } else  {     
-      const indicator_value =
-        description[data_config.column.indicator_type] === data_config.indicator_type.andel.db 
-          ? ind_per_unit[0][data_config.column.variable] < 0.1 
-            ? `${Math.round(ind_per_unit[0][data_config.column.variable] *1000 )/10}%` 
-            : `${Math.round(ind_per_unit[0][data_config.column.variable] *100 )}%` 
-          : `${ind_per_unit[0][data_config.column.variable]}`
-          
-      const denominator = ind_per_unit[0][data_config.column.denominator]
-      const numerator = Math.round(ind_per_unit[0][data_config.column.variable] * denominator)
+      if (ind_per_unit.length < 1){
+        return <td
+          key = {`${ind_id}_${tr_unit}_${index}_su`}
+          className = "selected_unit">
+            <NO_DATA/>
+        </td>
+      } else if (ind_per_unit[0][data_config.column.coverage_id] < 0.6){
+        //|| typeof(ind_per_unit[0][data_config.column.coverage_id]) === "undefined") {
+        return <td
+          key = {`${ind_id}_${tr_unit}_${index}_su`}
+          className = "selected_unit">
+            <LOW_COV/>
+        </td>
+      } else if (ind_per_unit[0][data_config.column.denominator] <
+        description[data_config.column.min_denominator]) {
+        return <td
+          key = {`${ind_id}_${tr_unit}_${index}_su`}
+          className = "selected_unit">
+            <LOW_N/>
+        </td>
+      } else  { 
+        const ind_type = description[data_config.column.indicator_type]
+        const level_class =  
+          (ind_per_unit[0][data_config.column.achieved_level] !== show_level_filter &&
+          show_level_filter !== null )
+          ? "filtered_level":""
 
-      const icon_class = ind_per_unit[0][data_config.column.achieved_level] === "H" 
-        ? "fa fa-fas fa-circle"
-        : ind_per_unit[0][data_config.column.achieved_level] === "M"
-        ? "fa fa-fas fa-adjust"
-        : ind_per_unit[0][data_config.column.achieved_level] === "L" 
-        ? "fa fa-circle-o"
-        : null
-      const level_class =  ind_per_unit[0][data_config.column.achieved_level] !== show_level_filter && show_level_filter !== null?
-        "filtered_level":""
-     return <INDICATOR_VALUE
-      key = {`${ind_id}_${tr_unit}_${index}_su`}
-        td_class = {`selected_unit ${level_class}`}
-        indicator_value = {indicator_value} 
-        icon_class = {icon_class}
-        share_of_totla = {numerator}
-        mid_text = {mid_text}
-        total = {denominator}
-      />;
-    }
-  })
+        return (
+          <INDICATOR_VALUE
+            key = {`${ind_id}_${tr_unit}_${index}_su`}
+            ind_type = {ind_type}
+            data = {ind_per_unit[0]}
+            td_class = {`selected_unit ${level_class}`}
+          />
+        )
+      }
+    })
   
-  const indicator_value_nation =
-    description[data_config.column.indicator_type] === data_config.indicator_type.andel.db ?
-      data.agg_data.nation.filtered_by_year[0][data_config.column.variable] < 0.1 ?
-        `${Math.round(data.agg_data.nation.filtered_by_year[0][data_config.column.variable] *1000 )/10}%` :
-        `${Math.round(data.agg_data.nation.filtered_by_year[0][data_config.column.variable]*100 )}%` :
-      `${data.agg_data.nation.filtered_by_year[0][data_config.column.variable]}`
-  const denominator_nation = data.agg_data.nation.filtered_by_year[0][data_config.column.denominator]
-  const numerator_nation = Math.round(data.agg_data.nation.filtered_by_year[0][data_config.column.variable] * denominator_nation)
-
-      const icon_class_nation = data.agg_data.nation.filtered_by_year[0][data_config.column.achieved_level] === "H" ?
-       "fa fa-fas fa-circle": 
-        data.agg_data.nation.filtered_by_year[0][data_config.column.achieved_level] === "M" ? 
-        "fa fa-fas fa-adjust":
-        data.agg_data.nation.filtered_by_year[0][data_config.column.achieved_level] === "L" ?
-         "fa fa-circle-o" :
-      ""
-  const level_class =  data.agg_data.nation.filtered_by_year[0][data_config.column.achieved_level] !==
-    show_level_filter && show_level_filter !== null ?
-    "filtered_level":""
   
-  const tr_fig =  selected_row === ind_id ?
-   <TR_FIGURE
-    colspan = {colspan}
-    data ={data}
-    figure_class = {med_field_class}
-    update_selected_row = {update_selected_row}
-  /> : null
+  const level_class =  
+    (data.agg_data.nation.filtered_by_year[0][data_config.column.achieved_level] !==
+    show_level_filter && show_level_filter !== null)
+    ? "filtered_level"
+    : ""
+  
+  const tr_fig =  selected_row === ind_id 
+  ? <TR_FIGURE
+      colspan = {colspan}
+      data ={data}
+      figure_class = {med_field_class}
+      update_selected_row = {update_selected_row}
+    /> 
+  : null
 
   const tr_click_handler = ()=> {
     if (selected_row === ind_id ){
@@ -137,17 +109,13 @@ function INDICATOR_ROW(props) {
         {indicator_val}
         <INDICATOR_VALUE
           key = {``}
+          data = {data.agg_data.nation.filtered_by_year[0]}
           td_class = {`nationally ${level_class}`}
-          indicator_value = {indicator_value_nation} 
-          icon_class = {icon_class_nation}
-          share_of_totla = {numerator_nation}
-          mid_text = {mid_text}
-          total = {denominator_nation}
         />
       </tr>
       {tr_fig}
     </>
-  )
+  );
 }
 
 export default INDICATOR_ROW;
