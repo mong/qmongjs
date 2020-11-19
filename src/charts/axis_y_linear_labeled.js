d90d22a0a4313bbaebaba8fdbcfc688192fee2f7
Lars-Jørgen Kristiansen <larsjk.84@gmail.com>
2020-11-19 22:31:39 +0100

    Run prettier on all files

--- src/charts/axis_y_linear_labeled.js
+++ src/charts/axis_y_linear_labeled.js
@@ -1,4 +1,4 @@
-import { axisRight, format } from 'd3'
+import { axisRight, format } from "d3";
 /** @module src/axis_y_linear_labeled */
 
 /**
@@ -14,12 +14,12 @@ export const labeled_y_axis_linear = function (selection, props) {
     y_scale,
     inner_width,
     inner_height,
-    y_axis_label = 'Y axis',
-    y_axis_label_fill = 'black',
+    y_axis_label = "Y axis",
+    y_axis_label_fill = "black",
     y_axis_label_offset = inner_width * 1.15,
-    y_axis_label_font_size = 7 + inner_width * 0.02 + 'px',
-    y_axis_label_font_family = 'arial',
-    y_axis_tick_font_size = 8 + inner_height * 0.02 + 'px',
+    y_axis_label_font_size = 7 + inner_width * 0.02 + "px",
+    y_axis_label_font_family = "arial",
+    y_axis_tick_font_size = 8 + inner_height * 0.02 + "px",
     y_axis_tick_number,
     y_axis_tick_font_fill,
     y_axis_tick_line_stroke,
@@ -28,43 +28,43 @@ export const labeled_y_axis_linear = function (selection, props) {
     transition = false,
     delay_val = 0,
     duration_val = 0,
-    axis_label_format = ',.0%'
-  } = props
+    axis_label_format = ",.0%",
+  } = props;
 
   const y_axis = axisRight(y_scale)
     .ticks(y_axis_tick_number)
     .tickSize(y_axis_tick_size)
-    .tickFormat(format(axis_label_format))
-  let y_axis_g = selection.selectAll('.y-axis').data([null])
+    .tickFormat(format(axis_label_format));
+  let y_axis_g = selection.selectAll(".y-axis").data([null]);
   y_axis_g = y_axis_g
     .enter()
-    .append('g')
-    .attr('class', 'y-axis')
-    .merge(y_axis_g)
+    .append("g")
+    .attr("class", "y-axis")
+    .merge(y_axis_g);
 
   // prettier-ignore
-  
+
   transition ? y_axis_g.transition().delay(delay_val).duration(duration_val).call(y_axis) : y_axis_g.call(y_axis)
 
   y_axis_g
-    .selectAll('.tick text')
-    .style('font-size', y_axis_tick_font_size)
-    .attr('fill', y_axis_tick_font_fill)
+    .selectAll(".tick text")
+    .style("font-size", y_axis_tick_font_size)
+    .attr("fill", y_axis_tick_font_fill);
 
-  y_axis_g.selectAll('.tick line').attr('stroke', y_axis_tick_line_stroke)
-  y_axis_g.select('.domain').attr('stroke', y_axis_domain_line_stroke)
+  y_axis_g.selectAll(".tick line").attr("stroke", y_axis_tick_line_stroke);
+  y_axis_g.select(".domain").attr("stroke", y_axis_domain_line_stroke);
 
-  const y_axis_label_text = y_axis_g.selectAll('.axis-label').data([null])
+  const y_axis_label_text = y_axis_g.selectAll(".axis-label").data([null]);
   y_axis_label_text
     .enter()
-    .append('text')
-    .attr('class', 'axis-label')
+    .append("text")
+    .attr("class", "axis-label")
     .merge(y_axis_label_text)
-    .attr('fill', y_axis_label_fill)
+    .attr("fill", y_axis_label_fill)
     .text(y_axis_label)
-    .attr('transform', 'rotate(-90)')
-    .attr('x', -inner_height / 1.5)
-    .attr('y', y_axis_label_offset)
-    .style('font-size', y_axis_label_font_size)
-    .style('font-family', y_axis_label_font_family)
-}
+    .attr("transform", "rotate(-90)")
+    .attr("x", -inner_height / 1.5)
+    .attr("y", y_axis_label_offset)
+    .style("font-size", y_axis_label_font_size)
+    .style("font-family", y_axis_label_font_family);
+};
