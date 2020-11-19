a0e087f47ffff13e295e1411df594b1a405eb8e1
Lars-Jørgen Kristiansen <LarsJK.84@gmail.com>
2020-11-19 13:46:19 +0100

    Refactor (#60)
    * Prettier app and select_multi
    
    * Install TS deps
    
    * Dont ignore package-lock
    
    * [WIP] TS
    
    * [WIP] TS
    
    * Add types for d3
    
    * tf_button ts migration
    
    * fixed enum for chart_type
    
    * migrated tu_list_body component to TS
    
    * migrated tu_list component to TS
    
    * migrated tu_list_header component to TS
    
    * WIP: convert to typescript
    
    * WIP: convert to typescript, debugging props
    
    * WIP: migrated some components to TS
    
    * WIP: convert to typescript
    
    * WIP: convert to typescript
    
    * WIP: Adjust props
    
    * WIP: Add types to line_chart
    
    * Refactor line_chart and tf_figure
    
    * Fix chart_type button
    
    * Fix chart_type button
    
    * Update src/charts/line_chart.ts
    
    Co-authored-by: Lars-Jørgen Kristiansen <LarsJK.84@gmail.com>
    
    Co-authored-by: Gabrio Frantzen <gabrio@jupiter.no>
    Co-authored-by: Kristian Bjørnstad <kristian@jupiter.no>

---
+++ src/components/select_multi.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import Select, { Styles } from "react-select";
+
+import { app_text } from "../app_config";
+
+interface OptsTu {
+  label: string;
+  options: {
+    value: string;
+    label: string;
+  }[];
+}
+
+interface Props {
+  opts: OptsTu[];
+  select_className?: string;
+  placeholder?: JSX.Element;
+  update_tu(x: string[]): void;
+  treatment_unit: string[];
+}
+
+function SELECT_MULTI(props: Props) {
+  const {
+    opts = [],
+    select_className = "pick_treatment_unit",
+    placeholder = (
+      <div>
+        <i className="fas fa-search"></i> Søk etter behandlingsenheter
+      </div>
+    ),
+    update_tu,
+    treatment_unit,
+  } = props;
+
+  let selection_options = opts;
+  const customStyles: Partial<Styles> = {
+    control: (provided, state) => ({
+      ...provided,
+      width: "100%",
+      backgroundColor: "#00263d",
+      boxShadow: state.isFocused ? undefined : undefined,
+      fontSize: "1rem",
+      border: "none",
+      borderRadius: state.isFocused ? 0 : 0,
+      borderBottom: state.isFocused
+        ? "3px solid #7ebec7"
+        : // @ts-ignore
+        state.isSelected
+        ? "3px solid #EEF6F7"
+        : "3px solid #EEF6F7",
+      cursor: "text",
+    }),
+    input: (provided) => ({
+      ...provided,
+      color: "#EEF6F7",
+      paddingLeft: "1.3rem",
+    }),
+    multiValue: (provided) => ({
+      ...provided,
+      color: "#00263d",
+      backgroundColor: "#EEF6F7",
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: "#EEF6F7",
+      fontSize: "1.2rem",
+    }),
+    // @ts-ignore
+    crossIcon: (provided) => ({
+      ...provided,
+      color: "white",
+    }),
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      display: "none",
+    }),
+    indicatorSeparator: (provided) => ({
+      ...provided,
+      display: "none",
+    }),
+    menu: (provided) => ({
+      ...provided,
+      zIndex: 3,
+    }),
+    option: (provided) => ({
+      ...provided,
+    }),
+  };
+  const value_tu = treatment_unit.map((tu) => ({ value: tu, label: tu }));
+  const handle_input_change = (e: { value: string }[]) => {
+    const tu = e !== null ? e.map((e) => e.value) : [];
+    update_tu(tu);
+  };
+
+  return (
+    <form>
+      <Select
+        className={select_className}
+        options={selection_options}
+        placeholder={placeholder}
+        closeMenuOnSelect={true}
+        value={value_tu}
+        openMenuOnClick={false}
+        isSearchable
+        isMulti={true}
+        onChange={(e: any) => handle_input_change(e)}
+        styles={customStyles}
+        menuIsOpen={
+          treatment_unit.length < app_text.tu_list.max_nr_tu ? undefined : false
+        }
+      />
+    </form>
+  );
+}
+
+export default SELECT_MULTI;
---
+++ src/components/utils.ts
@@ -0,0 +1,30 @@
+import { MutableRefObject, useEffect, useState } from "react";
+import ResizeObserver from "resize-observer-polyfill";
+
+function useResizeObserver(ref: MutableRefObject<HTMLElement | null>) {
+  const [dimensions, setDimensions] = useState<ResizeObserverEntry | null>(
+    null
+  );
+  useEffect(() => {
+    const observeTarget = ref?.current;
+
+    if (!observeTarget) {
+      return;
+    }
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        setDimensions(entry);
+      });
+    });
+
+    resizeObserver.observe(observeTarget);
+
+    return () => {
+      resizeObserver.unobserve(observeTarget);
+    };
+  }, [ref]);
+  return dimensions;
+}
+
+export default useResizeObserver;
