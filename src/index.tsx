38c3dbfab9965b5de577896873a20d1527f02942
Gabrio Frantzen <gabrio@jupiter.no>
2020-12-09 12:38:35 +0100

    Add react-router-dom, use-query-params and wrap App

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Route } from "react-router-dom";
+import { QueryParamProvider } from "use-query-params";
 
 loadDevTools(() =>
   ReactDOM.render(
     <React.StrictMode>
-      <App />,
+      <BrowserRouter>
+        <QueryParamProvider ReactRouterRoute={Route}>
+          <App />
+        </QueryParamProvider>
+      </BrowserRouter>
+      ,
     </React.StrictMode>,
     document.getElementById("root")
   )
