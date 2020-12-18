2932d5bb8b82632d64f4704ce5125bf6acfd497d
Gabrio Frantzen <gabrio@jupiter.no>
2020-12-18 11:52:45 +0100

    add back react-router-dom, as useQueryParam needs access to location history

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { QueryParamProvider } from "use-query-params";
+import { BrowserRouter, Route } from "react-router-dom";
 
 loadDevTools(() =>
   ReactDOM.render(
     <React.StrictMode>
-      <QueryParamProvider>
-        <App />
-      </QueryParamProvider>
+      <BrowserRouter>
+        <QueryParamProvider ReactRouterRoute={Route}>
+          <Route path="/">
+            <App />
+          </Route>
+        </QueryParamProvider>
+      </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
   )
