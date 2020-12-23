dc3af5f7bde9100dd09c668ccacda37e782ffffc
Gabrio Frantzen <gabrio@jupiter.no>
2020-12-23 11:46:30 +0100

    implement Sentry.io for exception tracking

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,18 @@ import loadDevTools from "./dev-tools/load";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
+
+Sentry.init({
+  dsn:
+    "https://40163899a6bc4f71a29e2a4e3e35e9ce@o489056.ingest.sentry.io/5568249",
+  autoSessionTracking: true,
+  integrations: [new Integrations.BrowserTracing()],
+  // https://docs.sentry.io/platforms/javascript/performance/sampling/
+  tracesSampleRate: 1.0,
+});
 
 loadDevTools(() =>
   ReactDOM.render(
