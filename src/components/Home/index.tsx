23b70b6fa6ca9498aa552107c020ee9e2090db11
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-02-26 13:09:17 +0100

    mv header and footer to app component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,16 +2,12 @@ import React from "react";
 
 import style from "./index.module.css";
 
-import Header from "../Header";
 import Main from "./Main";
-import Footer from "../Footer";
 
 const Home = () => {
   return (
     <div className={style.homeContainer}>
-      <Header />
       <Main />
-      <Footer />
     </div>
   );
 };
