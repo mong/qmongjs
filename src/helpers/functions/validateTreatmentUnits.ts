5d7431fcc7c37c810a0795bd7d81f469c025bf25
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-03-01 11:19:02 +0100

    use input options to validate selected treatment units

--- src/helpers/functions/validateTreatmentUnits.ts
+++ src/helpers/functions/validateTreatmentUnits.ts
@@ -1,14 +1,15 @@
-import { TreatmentUnit } from "../../components/RegisterPage";
-
 export const validateTreatmentUnits = function (
   treatment_units: string[],
-  valid_treatment_units: TreatmentUnit[]
+  valid_treatment_units: {
+    label: string;
+    options: { value: string; label: string }[];
+  }[]
 ) {
   return (
     (treatment_units
       ?.filter((x) =>
-        valid_treatment_units.some(
-          (tu) => [tu.hospital, tu.hf, tu.rhf].indexOf(x) !== -1
+        valid_treatment_units.some((tu) =>
+          tu.options.map((unit) => unit.value === x).some((x) => x)
         )
       )
       .slice(0, 5) as string[]) || []
