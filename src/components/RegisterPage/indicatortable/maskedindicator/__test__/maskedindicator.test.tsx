fb317af933ec03a5a02c36f4c72c9b8473412f06
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-05-07 10:04:03 +0200

    compoent for masked values

---
+++ src/components/RegisterPage/indicatortable/maskedindicator/__test__/maskedindicator.test.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import { MaskedIndicator } from "../";
+
+let container: any = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders Lav dg", () => {
+  act(() => {
+    render(<MaskedIndicator text="Lav dg" />, container);
+  });
+  expect(container.textContent).toBe("Lav dg");
+});
+
+
+it("renders Lav N", () => {
+  act(() => {
+    render(<MaskedIndicator text="Lav N" />, container);
+  });
+  expect(container.textContent).toBe("Lav N");
+});
+
+
+it("renders Ingen data", () => {
+  act(() => {
+    render(<MaskedIndicator text="Ingen Data" />, container);
+  });
+  expect(container.textContent).toBe("Ingen Data");
+});
---
+++ src/components/RegisterPage/indicatortable/maskedindicator/maskedindicator.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { MaskedIndicator, MaskedIndicatorProps } from "./";
+
+export default {
+  title: "Indicator/Masked Indicator Values",
+  component: MaskedIndicator,
+} as Meta;
+
+const Template: Story<MaskedIndicatorProps> = (args) => <MaskedIndicator {...args} />;
+
+export const LowCoverage = Template.bind({});
+LowCoverage.args = {
+  text: "Lav dg"
+};
+
+export const NoData = Template.bind({});
+NoData.args = {
+  text: "Ingen Data"
+};
+export const LowN = Template.bind({});
+LowN.args = {
+  text: "Lav N"
+};
