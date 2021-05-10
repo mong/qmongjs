39ea6cd16f42d598483af5a1aae3fc8cd392cfe4
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-05-10 12:48:03 +0200

    update

--- src/helpers/functions/validateTreatmentUnits.ts
+++ src/helpers/functions/validateTreatmentUnits.ts
@@ -5,15 +5,38 @@ export const validateTreatmentUnits = function (
     options: { value: string; label: string }[];
   }[]
 ) {
-  return (
-    (treatment_units
-      ?.filter((x) =>
-        valid_treatment_units.some((tu) =>
-          tu.options.map((unit) => unit.value === x).some((x) => x)
-        )
-      )
-      .slice(0, 5) as string[]) || []
-  );
+
+  if (!treatment_units || valid_treatment_units.length === 0) {
+    return []
+  }
+  const unitsHosp = treatment_units.filter(unit => {
+    const hosp = valid_treatment_units
+      .filter(opts => opts.label === "Sykehus")
+      .map(validHosp => validHosp.options.map(
+        hospName => hospName.value
+      ))
+    return hosp[0].includes(unit)
+  }).sort()
+
+  const unitsHF = treatment_units.filter(unit => {
+    const HF = valid_treatment_units
+      .filter(opts => opts.label === "HF")
+      .map(validHF => validHF.options.map(
+        HFName => HFName.value
+      ))
+    return HF[0].includes(unit)
+  }).sort()
+  const unitsRHF = treatment_units.filter(unit => {
+    const rhf = valid_treatment_units
+      .filter(opts => opts.label === "RHF")
+      .map(validRHF => validRHF.options.map(
+        RHFName => RHFName.value
+      ))
+    return rhf[0].includes(unit)
+  }).sort()
+  const unitsOut = unitsHosp.concat(unitsHF, unitsRHF)
+
+  return unitsOut
 };
 
 export default validateTreatmentUnits;
