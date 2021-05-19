e0a285462e19f11111fbaf9063cc600214ac4be0
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-05-19 15:26:47 +0200

    rm unused code

--- src/components/low_cov.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LOW_COV = () => {
-  return <div style={{ textAlign: "center", opacity: "0.3" }}>Lav dg</div>;
-};
-
-export default LOW_COV;
--- src/components/low_n.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LOW_N = () => {
-  return <div style={{ textAlign: "center", opacity: "0.3" }}>Lav N</div>;
-};
-
-export default LOW_N;
