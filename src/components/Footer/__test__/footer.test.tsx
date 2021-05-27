1037e310912c3f86aa54d7cfbd9443e23c3e8ad7
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-05-27 14:50:55 +0200

    format

--- src/components/Footer/__test__/footer.test.tsx
+++ src/components/Footer/__test__/footer.test.tsx
@@ -3,17 +3,16 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { QueryParamProvider } from "use-query-params";
 import { render } from "@testing-library/react";
 
-import Footer from '../';
+import Footer from "../";
 
 it("Footer renders", async () => {
-
   const { container } = render(
-    <BrowserRouter >
+    <BrowserRouter>
       <QueryParamProvider ReactRouterRoute={Route}>
         <Footer />
       </QueryParamProvider>
     </BrowserRouter>
-  )
+  );
 
-  expect(container).toMatchSnapshot()
-})
+  expect(container).toMatchSnapshot();
+});
--- src/components/Header/__test__/header.test.tsx
+++ src/components/Header/__test__/header.test.tsx
@@ -3,17 +3,16 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { QueryParamProvider } from "use-query-params";
 import { render } from "@testing-library/react";
 
-import Header from '../';
+import Header from "../";
 
 it("Footer renders", async () => {
-
   const { container } = render(
-    <BrowserRouter >
+    <BrowserRouter>
       <QueryParamProvider ReactRouterRoute={Route}>
         <Header />
       </QueryParamProvider>
     </BrowserRouter>
-  )
+  );
 
-  expect(container).toMatchSnapshot()
-})
+  expect(container).toMatchSnapshot();
+});
