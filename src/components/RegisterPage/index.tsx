2923312879bb4ade4c9647735dac3f1ce2914a8c
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-05-27 15:50:18 +0200

    use new api url

--- src/components/RegisterPage/index.tsx
+++ src/components/RegisterPage/index.tsx
@@ -51,7 +51,7 @@ export interface Description {
 
 export const API_HOST =
   process.env.REACT_APP_API_HOST ??
-  "http://qa-mong-api.eu-west-1.elasticbeanstalk.com";
+  "https://qa-mong-api.skde.org";
 
 export const RegisterPage: React.FC = () => {
   let { path } = useRouteMatch();
