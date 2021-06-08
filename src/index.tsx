7a0f9cc6d6f908e7b4676f2fe3a823b980aee47a
arnfinn <arnfinn.steindal@gmail.com>
2021-06-08 14:19:33 +0200

    npm run format

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,7 @@ import { QueryParamProvider } from "use-query-params";
 import { BrowserRouter, Route } from "react-router-dom";
 
 Sentry.init({
-  dsn:
-    "https://40163899a6bc4f71a29e2a4e3e35e9ce@o489056.ingest.sentry.io/5568249",
+  dsn: "https://40163899a6bc4f71a29e2a4e3e35e9ce@o489056.ingest.sentry.io/5568249",
   autoSessionTracking: true,
   integrations: [new Integrations.BrowserTracing()],
   // https://docs.sentry.io/platforms/javascript/performance/sampling/
