4281e4b151243ac8be9e0d31d893e999fd12b58e
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-09-06 11:19:58 +0200

    add beregnet_andel indicator type

--- src/components/RegisterPage/indicatortable/indicatorvalue/indicatorvalue.tsx
+++ src/components/RegisterPage/indicatortable/indicatorvalue/indicatorvalue.tsx
@@ -56,8 +56,7 @@ export const IndicatorValue: React.FC<IndicatorValueProps> = (props) => {
         </div>
       </td>
     );
-  }
-  if (indicatorData.type === "andel") {
+  } else if (indicatorData.type === "andel") {
     const denominator = indicatorData.denominator;
     const numerator = Math.round(indicatorData.var * denominator);
     const indicator_value_share =
@@ -83,6 +82,31 @@ export const IndicatorValue: React.FC<IndicatorValueProps> = (props) => {
           </h4>
         </div>
         <div className={style.summary}>{`${numerator} av ${denominator}`}</div>
+      </td>
+    );
+  } else if (indicatorData.type === "beregnet_andel") {
+    const indicator_value_share =
+      indicatorData.var < 0.1
+        ? `${Math.round(indicatorData.var * 1000) / 10}\u202f% `
+        : `${Math.round(indicatorData.var * 100)}\u202f% `;
+
+    return (
+      <td
+        className={
+          td_class === "selected_unit"
+            ? `${style.selected_unit} ${filter_level}`
+            : `${style.nationally} ${filter_level}`
+        }
+      >
+        <div
+          className={style.level}
+          aria-label={`Achieved level ${indicatorData.level}`}
+        >
+          <h4>
+            {`${indicator_value_share}`}
+            <i className={icon_class} />
+          </h4>
+        </div>
       </td>
     );
   }
