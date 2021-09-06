22cfde4c6b474c66267d4ab98fd1a3ad297fcb1e
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-09-06 12:37:49 +0200

    refactor

--- src/components/RegisterPage/indicatortable/indicatorvalue/indicatorvalue.tsx
+++ src/components/RegisterPage/indicatortable/indicatorvalue/indicatorvalue.tsx
@@ -31,7 +31,18 @@ export const IndicatorValue: React.FC<IndicatorValueProps> = (props) => {
       : indicatorData.level === "L"
       ? "fa fa-circle-o"
       : "undeined";
-  if (indicatorData.type === "dg_andel" || indicatorData.type === "dg") {
+  if (
+    indicatorData.type === "dg_andel" ||
+    indicatorData.type === "dg" ||
+    indicatorData.type === "beregnet_andel" ||
+    indicatorData.type === "andel"
+  ) {
+    const denominator =
+      indicatorData.type === "andel" ? indicatorData.denominator : 0;
+    const numerator =
+      indicatorData.type === "andel"
+        ? Math.round(indicatorData.var * denominator)
+        : 0;
     const indicator_value_share =
       indicatorData.var < 0.1
         ? `${Math.round(indicatorData.var * 1000) / 10}\u202f% `
@@ -54,59 +65,9 @@ export const IndicatorValue: React.FC<IndicatorValueProps> = (props) => {
             <i className={icon_class} />
           </h4>
         </div>
-      </td>
-    );
-  } else if (indicatorData.type === "andel") {
-    const denominator = indicatorData.denominator;
-    const numerator = Math.round(indicatorData.var * denominator);
-    const indicator_value_share =
-      indicatorData.var < 0.1
-        ? `${Math.round(indicatorData.var * 1000) / 10}\u202f% `
-        : `${Math.round(indicatorData.var * 100)}\u202f% `;
-
-    return (
-      <td
-        className={
-          td_class === "selected_unit"
-            ? `${style.selected_unit} ${filter_level}`
-            : `${style.nationally} ${filter_level}`
-        }
-      >
-        <div
-          className={style.level}
-          aria-label={`Achieved level ${indicatorData.level}`}
-        >
-          <h4>
-            {`${indicator_value_share}`}
-            <i className={icon_class} />
-          </h4>
-        </div>
-        <div className={style.summary}>{`${numerator} av ${denominator}`}</div>
-      </td>
-    );
-  } else if (indicatorData.type === "beregnet_andel") {
-    const indicator_value_share =
-      indicatorData.var < 0.1
-        ? `${Math.round(indicatorData.var * 1000) / 10}\u202f% `
-        : `${Math.round(indicatorData.var * 100)}\u202f% `;
-
-    return (
-      <td
-        className={
-          td_class === "selected_unit"
-            ? `${style.selected_unit} ${filter_level}`
-            : `${style.nationally} ${filter_level}`
-        }
-      >
-        <div
-          className={style.level}
-          aria-label={`Achieved level ${indicatorData.level}`}
-        >
-          <h4>
-            {`${indicator_value_share}`}
-            <i className={icon_class} />
-          </h4>
-        </div>
+        {indicatorData.type === "andel" && (
+          <div className="summary">{`${share_of_total} av ${total}`}</div>
+        )}
       </td>
     );
   }
