61e0787e6d26e337bf02c5d37fdcfb300bfb197f
arnfinn <arnfinn.steindal@gmail.com>
2021-12-02 13:31:12 +0100

    Rødt og grønt målnivå gitt i indikatorbeskrivelse
    Kun riktig for direction 1 foreløpig.

--- src/components/RegisterPage/indicatortable/chartrow/chartrowdescription.tsx
+++ src/components/RegisterPage/indicatortable/chartrow/chartrowdescription.tsx
@@ -3,18 +3,23 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Level } from "../../../Charts/types";
 
 interface Props {
   description_title?: string;
   description_text: string;
   delivery_time: Date;
+  levels: Level[];
 }
+
 
 const ChartRowDescription = ({
   description_text,
   description_title = "Om kvalitetsindikatoren",
   delivery_time,
+  levels,
 }: Props) => {
+  console.log(levels);
   return (
     <div className={style.description_container}>
       <Accordion>
@@ -23,6 +28,11 @@ const ChartRowDescription = ({
         </AccordionSummary>
         <AccordionDetails>
           {description_text}
+          <p>
+            Grønt målnivå over {Math.round(levels[0].end*100)} %
+            <br />
+            Rødt målnivå under {Math.round(levels[2].start*100)} %
+          </p>
           <p>
             Data ble sist oppdatert{" "}
             {delivery_time.toLocaleString("no-NO", {
