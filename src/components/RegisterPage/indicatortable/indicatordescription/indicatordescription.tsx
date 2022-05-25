45b5716e51eaa83c6d2044fcd0ff69a142f9d363
arnfinn <arnfinn.steindal@gmail.com>
2022-05-25 16:24:04 +0200

    rewrite some stuff

--- src/components/RegisterPage/indicatortable/indicatordescription/indicatordescription.tsx
+++ src/components/RegisterPage/indicatortable/indicatordescription/indicatordescription.tsx
@@ -17,18 +17,10 @@ export const IndicatorDescription: React.FC<DescriptionProps> = (props) => {
   const level_direction = description.level_direction;
   const level_green = description.level_green;
 
-  const LEVEL_TEXT =
-    level_green === null ? (
-      ""
-    ) : level_direction === 1 ? (
-      <h4>
-        ØNSKET MÅLNIVÅ: &#8805; {customFormat(numberFormat)(level_green)}{" "}
-      </h4>
-    ) : (
-      <h4>
-        ØNSKET MÅLNIVÅ: &#8804; {customFormat(numberFormat)(level_green)}{" "}
-      </h4>
-    );
+  const level_sign =
+    level_direction === 1
+      ? String.fromCodePoint(parseInt("2265", 16))
+      : String.fromCodePoint(parseInt("2264", 16));
 
   return (
     <td className={style.quality_indicator}>
@@ -38,7 +30,14 @@ export const IndicatorDescription: React.FC<DescriptionProps> = (props) => {
       <div className={style.qi_long_description}>
         <p>{short_description}</p>
       </div>
-      <div className={style.desired_target_level}>{LEVEL_TEXT}</div>
+      {level_green !== null && (
+        <div className={style.desired_target_level}>
+          <h4>
+            ØNSKET MÅLNIVÅ: {level_sign}{" "}
+            {customFormat(numberFormat)(level_green)}
+          </h4>
+        </div>
+      )}
     </td>
   );
 };
