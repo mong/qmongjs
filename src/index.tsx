819e059170d53698c945b77e73a14414d44d533a
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-05-31 13:48:38 +0200

    Dependabot updates (#1337)
    ## Major updates
    
    * Bump @testing-library/user-event from 13.5.0 to 14.2.0 (#1331)
    * Bump @sentry/tracing and @sentry/react from 6.19.7 to 7.0.0 (#1360)
    
    ## Other updates
    
    * mui to 5.8.0
    * Bump msw from 0.39.2 to 0.40.0 (#1335)
    * Bump @storybook/addon-essentials from 6.4.22 to 6.5.0 (#1338)
    * Bump @storybook/addon-essentials from 6.4.22 to 6.5.0
    * Bump @storybook/addon-links from 6.4.22 to 6.5.0 (#1339)
    * Bump @storybook/addon-links from 6.5.0 to 6.5.4 (#1349)
    * Bump @storybook/addon-links from 6.5.0 to 6.5.4
    * Bump @storybook/preset-create-react-app from 4.1.0 to 4.1.1 (#1347)
    * Bump eslint from 8.15.0 to 8.16.0 (#1346)
    * Bump msw from 0.40.0 to 0.41.0 (#1345)
    * Bump @types/d3 from 7.1.0 to 7.4.0 (#1344)
    * Bump @mui/material from 5.8.0 to 5.8.1 (#1350)
    * Bump @storybook/addon-essentials from 6.5.0 to 6.5.4 (#1348)
    * Bump @storybook/addon-links from 6.5.4 to 6.5.5 (#1355)
    * Bump lint-staged from 12.4.1 to 12.4.2 (#1354)
    * Bump @storybook/addon-essentials from 6.5.4 to 6.5.6 (#1356)
    * Bump @storybook/addon-links from 6.5.5 to 6.5.6 (#1363)
    * Bump msw from 0.41.0 to 0.41.1 (#1362)
    * Bump lint-staged from 12.4.2 to 12.4.3 (#1361)
    * Bump react-icons from 4.3.1 to 4.4.0 (#1366)
    * Bump typedoc from 0.22.15 to 0.22.16 (#1364)
    * Bump react-query from 3.39.0 to 3.39.1 (#1358)
    * Bump msw from 0.41.0 to 0.42.0 (#1365)
    * Bump typescript from 4.6.4 to 4.7.2 (#1367)

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,13 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import "./index.css";
 import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
 
 Sentry.init({
   dsn: "https://40163899a6bc4f71a29e2a4e3e35e9ce@o489056.ingest.sentry.io/5568249",
   autoSessionTracking: true,
-  integrations: [new Integrations.BrowserTracing()],
+  integrations: [new BrowserTracing()],
   // https://docs.sentry.io/platforms/javascript/performance/sampling/
   tracesSampleRate: 1.0,
 });
