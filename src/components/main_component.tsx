16d05eeb9ba4c90ec802013c24e1b73a82bef691
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-06-27 14:29:30 +0200

    Moved legends.js to TargetLevels component (#1434)

--- src/components/main_component.tsx
+++ src/components/main_component.tsx
@@ -7,7 +7,7 @@ import {
 } from "../components/RegisterPage";
 import { mainQueryParamsConfig } from "../app_config";
 
-import LEGEND from "./legend";
+import LEGEND from "./TargetLevels";
 //import Loading from "./Loading.tsx";
 import { MedicalFiedls } from "./RegisterPage/medicalfields";
 import { IndicatorTable } from "./RegisterPage/indicatortable";
