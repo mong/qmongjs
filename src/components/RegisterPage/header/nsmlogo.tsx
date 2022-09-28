82e22b18f5a1c56905ec0f3b4d54f15ee4f635b5
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-09-28 15:07:19 +0200

    Use <img> instead of <Image> (#1601)
    next export will not work with <Image>

--- src/components/RegisterPage/header/nsmlogo.tsx
+++ src/components/RegisterPage/header/nsmlogo.tsx
@@ -1,16 +1,11 @@
-import Image from "next/image";
-import logo from "../../../../public/img/nsmlogo.svg";
 import style from "./nsmlogo.module.css";
 
 export const NSMLogo: React.FC = () => {
+  const logo = "/img/nsmlogo.svg";
   return (
     <div className={style.nsmlogoWrapper}>
       <a href="https://www.kvalitetsregistre.no/">
-        <Image
-          className={style.nsmlogo}
-          src={logo}
-          alt="kvalitetsregistre.no"
-        />
+        <img className={style.nsmlogo} src={logo} alt="kvalitetsregistre.no" />
       </a>
     </div>
   );
