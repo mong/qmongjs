1088ddc4eaf5122961b97bca53d4c24625f7be4b
arnfinn <arnfinn.steindal@gmail.com>
2022-10-05 11:11:43 +0200

    Fetch registry names before export

--- pages/[register]/[tab].tsx
+++ pages/[register]/[tab].tsx
@@ -1,6 +1,10 @@
 import { UseQueryResult } from "react-query";
 import SelectedRegister from "../../src/components/RegisterPage/SelectedRegister";
-import { useRegisterNamesQuery } from "../../src/helpers/hooks";
+import {
+  useRegisterNamesQuery,
+  fetchRegisterNames,
+} from "../../src/helpers/hooks";
+import { GetStaticProps, GetStaticPaths } from "next";
 
 const SelectedRegisterPage = () => {
   const registryNameQuery: UseQueryResult<any, unknown> =
@@ -13,3 +17,31 @@ const SelectedRegisterPage = () => {
 };
 
 export default SelectedRegisterPage;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  return {
+    props: { content: [] },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const registries = await fetchRegisterNames();
+  const paths = registries.flatMap((registry: any) => {
+    const { caregiver_data, resident_data, dg_data } = registry;
+    const sykehustab =
+      caregiver_data === 1
+        ? [{ params: { register: registry.rname, tab: "sykehus" } }]
+        : [];
+    const opptaktab =
+      resident_data === 1
+        ? [{ params: { register: registry.rname, tab: "opptaksomraade" } }]
+        : [];
+    const kvalitettab =
+      dg_data === 1
+        ? [{ params: { register: registry.rname, tab: "datakvalitet" } }]
+        : [];
+    return [...sykehustab, ...opptaktab, ...kvalitettab];
+  });
+
+  return { paths, fallback: false };
+};
